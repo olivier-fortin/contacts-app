c3ac2c26c182144f472778e08be29c14f4bbaece
Olivier Fortin <fortin.olivier@gmail.com>
2017-11-18 10:57:22 -0500

    Add front structure

---
+++ assets/js/contacts/contacts.app.js
@@ -0,0 +1,23 @@
+// Main object
+var contacts = window.contacts || {};
+
+contacts.App = function (document, $, console) {
+
+    'use strict';
+
+    var init = function () {
+
+        //Prevent double click on mobile
+        $('a:not([target="_blank"])').on('click touchend', function (e) {
+            var el = $(this);
+            window.location = el.attr('href');
+        });
+
+    };
+
+    return {
+        init: init
+    };
+
+}(window.document, window.jQuery, window.console);
+window.jQuery(window.document).on('ready', contacts.App.init);
