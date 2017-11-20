3de313fd29dfb036064f8a371b3ae4d3e6e98076
Olivier Fortin <fortin.olivier@gmail.com>
2017-11-19 19:26:56 -0500

    Add detail view when profile is clicked

--- assets/js/contacts/contacts.loadContactList.js
+++ assets/js/contacts/contacts.loadContactList.js
@@ -13,8 +13,9 @@ contacts.loadContactList = function (document, $, console) {
             var listGroup = $(".list-group");
 
             for (var i = 0; i < json.length; i++) {
+
                 listGroup.append(
-                    "<li class='list-group-item'>" + json[i].id +
+                    "<li class='list-group-item' id='" + json[i].id + "'>" +
                     "<img class='img-circle' src=" + json[i].picture + ">" +
                     "<div class='profile-info'>\n" +
                     "<h4 class='list-group-item-heading'>" + json[i].firstName + " " + json[i].lastName + "</h4>" +
@@ -23,8 +24,28 @@ contacts.loadContactList = function (document, $, console) {
                     "</li>"
                 );
             }
+
+            $(".list-group-item").click(function() {
+                $(".contact-edit").hide();
+                $(".contact-details").show();
+
+                var id = $(this).attr('id');
+                var currentContact = json[id-1];
+
+                alert("Clicked"+ id);
+                console.log(currentContact);
+
+                $(".contact-image").attr("src", currentContact.picture );
+                $(".contact-name").html( currentContact.firstName + " " + currentContact.lastName);
+                $(".contact-job-title").html( currentContact.jobTitle );
+                $(".contact-address").html( currentContact.address.street + ", " + currentContact.address.city + "<br>" + currentContact.address.state + ", " + currentContact.address.postcode );
+                $(".contact-phone").html( currentContact.phoneNumber );
+                $(".contact-cell").html( currentContact.cellNumber );
+                $(".contact-email").html( currentContact.email );
+            });
         });
     }
+
     loadContactList();
 
 }(window.document, window.jQuery, window.console);
